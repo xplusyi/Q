2deb04c716cdc5dda1902c5d21914e15da9cf7c6
x+yi <24505459+xplusyi@users.noreply.github.com>
2024-11-25 23:33:07 +0800

    Update quartz.config.ts

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🏡 X+Yi",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
